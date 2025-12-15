939c600be7117cf9b338a672c1d564cce7449932
Jortizuwu <joseher2511@gmail.com>
2025-12-14 20:52:23 -0500

    added sonner and env .gitignore

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./core/app";
 import QueryProvider from "./shared/components/providers/react-query";
+import { Toaster } from "@/components/ui/sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryProvider>
       <App />
+      <Toaster />
     </QueryProvider>
   </StrictMode>
 );
