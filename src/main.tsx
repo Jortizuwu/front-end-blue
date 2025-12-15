c54552333dc148c69b1840f7171cf5fd9386a680
Jortizuwu <joseher2511@gmail.com>
2025-12-14 20:45:35 -0500

    added react query provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import "./index.css";
 import { App } from "./core/app";
+import QueryProvider from "./shared/components/providers/react-query";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryProvider>
+      <App />
+    </QueryProvider>
   </StrictMode>
 );
